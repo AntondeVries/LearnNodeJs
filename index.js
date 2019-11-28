ca87aa8a5e381bc6d17ee402ba9e0cc2c999d7ce
Anton de Vries <anton_devries@yahoo.de>
2019-11-28 14:55:35 +0100

    NodeJsChrashCourse

---
+++ index.js
@@ -0,0 +1,3 @@
+const Person = require('./person');
+const person1 = new Person('Anton' , 30);
+person1.greeting();
---
+++ person.js
@@ -0,0 +1,11 @@
+class Person{
+    constructor(name , age){
+        this.age = age;
+        this.name = name;
+    }
+    greeting(){
+        console.log(`my name is ${this.name} and i am ${this.age}`)
+    }
+}
+
+module.exports = Person;
